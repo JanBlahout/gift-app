86a2816a5ff95f4dc8c8209b81272068e7e3a3ae
Jan Blahout <jan.blahout@outlook.com>
2024-03-18 14:22:15 +0100

    better landing page when there are no gifts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { useQuery } from 'convex/react';
 import { api } from '../../convex/_generated/api';
 import { UploadButton } from './upload-button';
 import GiftCard from './gift-card';
+import Image from 'next/image';
 
 export default function Home() {
   const organization = useOrganization();
@@ -19,11 +20,25 @@ export default function Home() {
 
   return (
     <main className="container pt-12">
-      <div className="flex justify-between items-center mb-10">
-        <h1 className="text-4xl font-bold ">Gifts</h1>
-        <UploadButton />
-      </div>
+      {gifts && gifts?.length === 0 && (
+        <div className="flex flex-col gap-4 w-full items-center mt-24">
+          <Image
+            alt="picture of a gift and a woman"
+            width="400"
+            height="400"
+            src="/no_gifts.svg"
+          />
+          <h3 className="text-2xl">There are no gifts on the wishlist 😥</h3>
+          <UploadButton />
+        </div>
+      )}
 
+      {gifts && gifts.length > 0 && (
+        <div className="flex justify-between items-center mb-10">
+          <h1 className="text-4xl font-bold ">Gifts</h1>
+          <UploadButton />
+        </div>
+      )}
       <div className="grid grid-cols-4 gap-4">
         {gifts?.map((gift) => {
           return <GiftCard key={gift._id} gift={gift} />;
