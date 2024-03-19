657c9f7dbe2475c536217a4126d362691dff03ce
Jan Blahout <jan.blahout@outlook.com>
2024-03-19 15:37:10 +0100

    change gifts layout

--- src/app/gifts/layout.tsx
+++ src/app/gifts/layout.tsx
@@ -4,7 +4,7 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main className="container mx-auto pt-12 min-h-screen">
+    <main className="container mx-auto pt-12 min-h-fit">
       <div className="flex gap-8">
         <div className="w-full">{children}</div>
       </div>
